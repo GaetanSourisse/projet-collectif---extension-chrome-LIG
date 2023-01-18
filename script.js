61a613803618773ee6ac4eb4d037859ec90fb441
gaetan sourisse <USER@MacBook-Air.local>
2023-01-18 16:21:46 +0100

    première version + loupe

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-console.log("Hello");
 let loupe = document.createElement('div');
 loupe.setAttribute('id','loupe');
+document.body.appendChild(loupe);
+
+let zoom = 1
+document.body.onmousemove = function(){
+    loupe.style.left = event.clientX + "px";
+    loupe.style.top = event.clientY + "px";
+    loupe.style.backgroundSize = (1000 * zoom) + "px";
+    loupe.style.backgroundPosition = ((-loupe.offsetLeft * zoom) - 150) + "px " + ((-loupe.offsetTop * zoom)-150) + "px ";
+}
