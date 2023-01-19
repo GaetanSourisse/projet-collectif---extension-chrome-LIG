26fe14d976cdaf897c121f36fb01daa9b516a5b8
Gaetan Sourisse <gaetansourisse@hotmail.fr>
2023-01-19 12:07:47 +0100

    version bouton actif popup

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+console.log("prout");
 let loupe = document.createElement('div');
 loupe.setAttribute('id','loupe');
 document.body.appendChild(loupe);
-
+console.log("pi");
 let zoom = 1
 document.body.onmousemove = function(){
     loupe.style.left = event.clientX + "px";
@@ -9,3 +10,26 @@ document.body.onmousemove = function(){
     loupe.style.backgroundSize = (1000 * zoom) + "px";
     loupe.style.backgroundPosition = ((-loupe.offsetLeft * zoom) - 150) + "px " + ((-loupe.offsetTop * zoom)-150) + "px ";
 }
+
+
+let extensionActive = false;
+let toggle = document.querySelector(".toggle");
+console.log(toggle);
+
+toggle.addEventListener("click", () => {
+    console.log("je suis bien là");
+    if(extensionActive == false){
+        console.log("extension activée");
+        extensionActive = true;
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        document.body.style.fontSize = "xx-large";
+
+        
+    } else {
+        console.log("extension fermée");
+        loupe.style.display = "none";
+        extensionActive = false;
+
+    }
+})
