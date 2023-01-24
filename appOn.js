36439dec95770a2a706435803771af6bb5eae02c
Lorena <lorenacamposrosas@gmail.com>
2023-01-24 10:14:26 +0100

    erreur ne suit pas la souris
    This reverts commit 743bf0fc93a358b6bebe896328eecdef0318eac7.

--- appOn.js
+++ appOn.js
@@ -37,51 +37,29 @@ function contrast (){
     })
 }
 
-
 function createLoupe (){
     // creation de la loupe
     let loupe = document.createElement('div');
     loupe.setAttribute('id','loupe');
     document.body.appendChild(loupe);
+
+    // css de la loupe
+    loupe.style.width = "100px"
+    loupe.style.height = "100px"
+    loupe.style.borderRadius = "60px"
+    loupe.style.border = "solid 3px rgb(212, 8, 42)"
+    loupe.style.position = "absolute"
+
+    // zoom et mouvement de la loupe
+    let zoom = 1
+    document.body.onmousemove = function(){
+        loupe.style.left = event.clientX + "px";
+        loupe.style.top = event.clientY + "px";
+        loupe.style.backgroundSize = (1000 * zoom) + "px";
+        loupe.style.backgroundPosition = ((-loupe.offsetLeft * zoom) - 150) + "px " + ((-loupe.offsetTop * zoom) - 150) + "px ";
+    }
 }
 
-
-// css de la loupe
-loupe.style.width = "100px"
-loupe.style.height = "100px"
-loupe.style.borderRadius = "60px"
-loupe.style.border = "solid 3px rgb(212, 8, 42)"
-loupe.style.position = "fixed"
-loupe.style.left= "-100%"
-loupe.style.top= "50%"
-loupe.style.transition = "all 300ms ease-out"
-loupe.style.transform= "translateX(-90%) translateY(-90%)"
-loupe.style.pointerEvents = "none"
-document.body.style.Cursor = "none"
-
-
-
-// zoom et mouvement de la loupe
-let zoom = 1
-document.body.addEventListener("mousemove", function(e){
-    loupe.style.left = e.clientX + "px";
-    loupe.style.top = e.clientY + "px";
-    loupe.style.backgroundSize = (1000 * zoom) + "px";
-    loupe.style.backgroundPosition = ((-loupe.offsetLeft * zoom) - 150) + "px " + ((-loupe.offsetTop * zoom) - 150) + "px ";
-});
-
-
-// fonction pour que la loupe suit la souris
-document.onmousemove = suitsouris;
-
-function suitsouris(evenement) {
-  if (navigator.appName == "Microsoft Internet Explorer") {
-    var x = event.x + document.body.scrollLeft;
-    var y = event.y + document.body.scrollTop;
-  } else {
-    var x = evenement.pageX;
-    var y = evenement.pageY;
-  }
-  document.getElementById("loupe").style.left = (x + 1) + 'px';
-  document.getElementById("loupe").style.top = (y + 1) + 'px';
+function deleteLoupe (){
+    loupe.style.display = "none"
 }
